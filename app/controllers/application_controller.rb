975dc52ad986d9f3bb37f26b872426630c22a0d1
SamirSir <acharyasamir18@gmail.com>
2021-02-06 01:01:35 +0545

    Added the notification panel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,28 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+before_action :authenticate_user!
 
-    def raw_data(query) 
-        return ActiveRecord::Base.connection.exec_query(query).rows
-    end
-
-    def labelled_data(query)
-        return ActiveRecord::Base.connection.execute(query)[0]
-    end
+def raw_data(query)
+    ActiveRecord::Base.connection.exec_query(query).rows
 end
 
+def labelled_data(query)
+    ActiveRecord::Base.connection.execute(query)[0]
+end
 
+# GROUP and AMount
+# ActiveRecord::Base.connection.exec_query("SELECT USER_ID, SUM(AMOUNT) AS TOTAL_AMOUNT FROM INCOMES WHERE GROUP_ID = 7 GROUP BY USER_ID")
+
+#  ActiveRecord::Base.connection.exec_query("SELECT USER_ID, SUM(AMOUNT) AS MAX_AMOUNT FROM INCOMES GROUP BY USER_ID ORDER BY MAX_AMOUNT DE
+# SC")[0]
+def max_of(isel, group_id)
+    raw_data("SELECT USER_ID, SUM(AMOUNT) AS MAX_AMOUNT FROM #{isel} WHERE GROUP_ID = #{group_id} GROUP BY USER_ID ORDER BY MAX_AMOUNT DESC")[0]
+end
+
+def min_of(isel, group_id)
+    raw_data("SELECT USER_ID, SUM(AMOUNT) AS MIN_AMOUNT FROM #{isel} WHERE GROUP_ID = #{group_id} GROUP BY USER_ID ORDER BY MIN_AMOUNT")[0]
+end
+
+def get_formatted_date(dateTime)
+    dateTime.strftime('%Y-%m-%d')
+end
+end
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,26 +1,42 @@
+
 class ReportsController < ApplicationController
   include ReportsHelper
-  before_action :set_group
+  before_action :set_group, only: [:notifications]
 
   require 'will_paginate/array'
 
-  def index
-  end
+  def index; end
 
   def notifications
-    # GROUP and AMount
-    # ActiveRecord::Base.connection.exec_query("SELECT USER_ID, SUM(AMOUNT) AS TOTAL_AMOUNT FROM INCOMES WHERE GROUP_ID = 7 GROUP BY USER_ID")
+    @best_incomer = max_of('INCOMES', @group.id)
+    @worst_incomer = min_of('INCOMES', @group.id)
 
-    #  ActiveRecord::Base.connection.exec_query("SELECT USER_ID, SUM(AMOUNT) AS MAX_AMOUNT FROM INCOMES GROUP BY USER_ID ORDER BY MAX_AMOUNT DE
-    # SC")[0]
-    
+    @best_saver = max_of('SAVINGS', @group.id)
+    @worst_saver = min_of('SAVINGS', @group.id)
+
+    @best_loaner = min_of('LOANS', @group.id)
+    @worst_loaner = max_of('LOANS', @group.id)
+
+    @best_expenser = min_of('EXPENSES', @group.id)
+    @worst_expenser = max_of('EXPENSES', @group.id)
+
+    @notices = {
+      'Best Incomer' => @best_incomer,
+      'Worst Incomer' => @worst_incomer,
+      'Best Saver' => @best_saver,
+      'Worst Saver' => @worst_saver,
+      'Best Loaner' => @best_loaner,
+      'Worst Loaner' => @worst_loaner,
+      'Best Expenser' => @best_expenser,
+      'Worst Expenser' => @worst_expenser
+    }
+
+    @notices = @notices.to_a.paginate(page: params[:page], per_page: 2)
   end
 
-  private 
+  private
 
   def set_group
-    @group = Group.find(params[:id]) 
+    @group = Group.find(params[:id])
   end
-
 end
-
